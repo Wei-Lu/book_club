7f6c634ddf364947ea03d327612a06cc069648c0
Wei Lu <wei_lu@acl.com>
2013-07-19 09:58:45 -0700

    heroku mail2

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,8 +7,8 @@ class ReviewsController < BooksController
    @review.book = @book
    @review.user = current_user
    if @review.save
-#     BooksMailer.delay.review_notification(current_user, @book)
-     BooksMailer.review_notification(current_user, @book).deliver
+     BooksMailer.delay.review_notification(current_user, @book)
+#     BooksMailer.review_notification(current_user, @book).deliver
      redirect_to @book,notice: "Review created successfully!"
    else
      redirect_to @book, notice: "Review creation failed."
